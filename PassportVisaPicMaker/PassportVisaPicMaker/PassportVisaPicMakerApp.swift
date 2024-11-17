b594fae9d5e0b9ad86218c72d3f7be8ce56beb6f
Jaswinder Singh <jaswindersinghjagga@gmail.com>
2024-11-16 20:51:39 -0500

    setiting

---
+++ PassportVisaPicMaker/PassportVisaPicMaker/PassportVisaPicMakerApp.swift
@@ -0,0 +1,17 @@
+//
+//  PassportVisaPicMakerApp.swift
+//  PassportVisaPicMaker
+//
+//  Created by Jaswinder SINGH on 2024-11-16.
+//
+
+import SwiftUI
+
+@main
+struct PassportVisaPicMakerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ PassportVisaPicMaker/PassportVisaPicMakerTests/PassportVisaPicMakerTests.swift
@@ -0,0 +1,17 @@
+//
+//  PassportVisaPicMakerTests.swift
+//  PassportVisaPicMakerTests
+//
+//  Created by Jaswinder SINGH on 2024-11-16.
+//
+
+import Testing
+@testable import PassportVisaPicMaker
+
+struct PassportVisaPicMakerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
